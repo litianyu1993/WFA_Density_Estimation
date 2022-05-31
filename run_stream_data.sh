1b9fc44ec0551e6d316504720f7684bea86cbe1c
Tianyu Li <tianyu.li@mail.mcgill.ca>
2022-05-31 19:24:41 -0400

    update running script

--- run_stream_data.sh
+++ run_stream_data.sh
@@ -1,6 +1,6 @@
 for exp_data in covType poker rialto sea elec mixeddrift hyperplane chess weather
     do
-    for method in gru
+    for method in wfa
     do
     python stream_sgd_wfa.py --exp_data $exp_data --method $method
     done
