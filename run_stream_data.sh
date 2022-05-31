0fe2ee478df8a18f5a544deac6412774bde27a6a
Tianyu Li <tianyu.li@mail.mcgill.ca>
2022-05-31 19:26:26 -0400

    adding gpu specification to sh

--- run_stream_data.sh
+++ run_stream_data.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-03:00
+#SBATCH --output=%N-%j.out
+source $ENV/env/bin/activate
+
+
 for exp_data in covType poker rialto sea elec mixeddrift hyperplane chess weather
     do
     for method in wfa
