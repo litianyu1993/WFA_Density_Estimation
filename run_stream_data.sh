7627e3938e336dfb8b587349a38fd94f7459ff97
Tianyu Li <litianyu@cedar5.cedar.computecanada.ca>
2022-06-02 09:10:42 -0700

    updated compute canada sh file

--- run_stream_data.sh
+++ run_stream_data.sh
@@ -3,15 +3,22 @@
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-03:00
-#SBATCH --output=%N-%j.out
-source $ENV/env/bin/activate
+#SBATCH --output=Slurm%j.out
+#SBATCH --account=def-bengioy
+module load scipy-stack
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index -r requirements.txt
+
 
 
 for exp_data in covType poker rialto sea elec mixeddrift hyperplane chess weather
     do
     for method in wfa
     do
-    python stream_sgd_wfa.py --exp_data $exp_data --method $method
+    python stream_sgd_wfa_windowed.py --exp_data $exp_data --method $method
     done
 done
 
--- run_stream_data_gru.sh
+++ run_stream_data_gru.sh
@@ -1,8 +1,22 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-03:00
+#SBATCH --output=Slurm%j.out
+#SBATCH --account=def-bengioy
+module load scipy-stack
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index -r requirements.txt
+
 for exp_data in covType poker rialto sea elec mixeddrift hyperplane chess weather
     do
     for method in gru
     do
-    python stream_sgd_wfa.py --exp_data $exp_data --method $method
+    python stream_sgd_wfa_windowed.py --exp_data $exp_data --method $method
     done
 done
 
