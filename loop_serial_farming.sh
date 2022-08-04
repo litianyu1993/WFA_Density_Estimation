385ea8b44d1e85fa673fec1431755fccf9a638d0
Tianyu Li <tianyu.li@mail.mcgill.ca>
2022-08-04 10:38:09 -0400

    adding gru and lstm for serial farming

---
+++ loop_serial_farming.sh
@@ -0,0 +1,29 @@
+export TABLE=wfa_table.dat
+
+N_cases=$(cat "$TABLE" | wc -l)
+
+for ((i=1; i<=$N_cases; i++))
+ do # Using environment variable I_FOR to communicate the case number to individual jobs:
+  export I_FOR=$i 
+  sbatch run_stream_data.sh
+ done
+
+export TABLE=gru_table.dat
+
+N_cases=$(cat "$TABLE" | wc -l)
+
+for ((i=1; i<=$N_cases; i++))
+ do # Using environment variable I_FOR to communicate the case number to individual jobs:
+  export I_FOR=$i
+  sbatch run_stream_data.sh
+ done
+
+ export TABLE=lstm_table.dat
+
+N_cases=$(cat "$TABLE" | wc -l)
+
+for ((i=1; i<=$N_cases; i++))
+ do # Using environment variable I_FOR to communicate the case number to individual jobs:
+  export I_FOR=$i
+  sbatch run_stream_data.sh
+ done
--- loop_wfa.sh
+++
@@ -1,9 +0,0 @@
-export TABLE=wfa_table.dat
-
-N_cases=$(cat "$TABLE" | wc -l)
-
-for ((i=1; i<=$N_cases; i++))
- do # Using environment variable I_FOR to communicate the case number to individual jobs:
-  export I_FOR=$i 
-  sbatch run_stream_data.sh
- done
