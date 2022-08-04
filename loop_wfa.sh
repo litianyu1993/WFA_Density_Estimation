3c2bb0a9a7ced94bc44531a988dc361ce592350a
Tianyu Li <tianyu.li@mail.mcgill.ca>
2022-08-04 10:32:17 -0400

    fixing sh file

--- loop_wfa.sh
+++ loop_wfa.sh
@@ -4,5 +4,5 @@ N_cases=$(cat "$TABLE" | wc -l)
 
 for ((i=1; i<=$N_cases; i++))
  do # Using environment variable I_FOR to communicate the case number to individual jobs:
-  export I_FOR=$i sbatch job_script.sh
+  export I_FOR=$i sbatch run_stream_data.sh
  done
