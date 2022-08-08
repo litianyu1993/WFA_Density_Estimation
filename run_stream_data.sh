27d4708bc45df0599ae582d245cb3e40c57fd8ca
Tianyu Li <litianyu@cedar5.cedar.computecanada.ca>
2022-08-08 06:35:26 -0700

    changing running file

--- run_stream_data.sh
+++ run_stream_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
-#SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --cpus-per-task=3  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=Slurm%j.out
 #SBATCH --account=def-bengioy
 module load scipy-stack
