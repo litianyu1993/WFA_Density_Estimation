f58b1e56feab1e8b3b7c33f46f63b303c0bb0c1e
Tianyu Li <tianyu.li@mail.mcgill.ca>
2023-01-10 13:49:20 -0500

    change serial farming

--- loop_serial_farming.sh
+++ loop_serial_farming.sh
@@ -1,34 +1,44 @@
-export TABLE=wfa_table.dat
+#export TABLE=wfa_table.dat
+#
+#N_cases=$(cat "$TABLE" | wc -l)
+#
+#for ((i=1; i<=$N_cases; i++))
+# do # Using environment variable I_FOR to communicate the case number to individual jobs:
+#  export I_FOR=$i
+#  sbatch run_stream_data.sh
+# done
+#
+#export TABLE=gru_table.dat
+#
+#N_cases=$(cat "$TABLE" | wc -l)
+#
+#for ((i=1; i<=$N_cases; i++))
+# do # Using environment variable I_FOR to communicate the case number to individual jobs:
+#  export I_FOR=$i
+#  sbatch run_stream_data.sh
+# done
+#
+# export TABLE=lstm_table.dat
+#
+#N_cases=$(cat "$TABLE" | wc -l)
+#
+#for ((i=1; i<=$N_cases; i++))
+# do # Using environment variable I_FOR to communicate the case number to individual jobs:
+#  export I_FOR=$i
+#  sbatch run_stream_data.sh
+# done
+#
+#  export TABLE=no_rec_table.dat
+#
+#N_cases=$(cat "$TABLE" | wc -l)
+#
+#for ((i=1; i<=$N_cases; i++))
+# do # Using environment variable I_FOR to communicate the case number to individual jobs:
+#  export I_FOR=$i
+#  sbatch run_stream_data.sh
+# done
 
-N_cases=$(cat "$TABLE" | wc -l)
-
-for ((i=1; i<=$N_cases; i++))
- do # Using environment variable I_FOR to communicate the case number to individual jobs:
-  export I_FOR=$i 
-  sbatch run_stream_data.sh
- done
-
-export TABLE=gru_table.dat
-
-N_cases=$(cat "$TABLE" | wc -l)
-
-for ((i=1; i<=$N_cases; i++))
- do # Using environment variable I_FOR to communicate the case number to individual jobs:
-  export I_FOR=$i
-  sbatch run_stream_data.sh
- done
-
- export TABLE=lstm_table.dat
-
-N_cases=$(cat "$TABLE" | wc -l)
-
-for ((i=1; i<=$N_cases; i++))
- do # Using environment variable I_FOR to communicate the case number to individual jobs:
-  export I_FOR=$i
-  sbatch run_stream_data.sh
- done
-
-  export TABLE=no_rec_table.dat
+export TABLE=RNADE.dat
 
 N_cases=$(cat "$TABLE" | wc -l)
 
